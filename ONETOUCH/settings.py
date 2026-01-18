4c33cd5da2d3ffdd82ba1a188d5f0b8c270ed7eb
Ruhaan Hawaldar <ruhaanhawaldar@gmail.com>
2026-01-18 15:41:53 +0530

    UPDATE

--- ONETOUCH/settings.py
+++ ONETOUCH/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h1n5+2*l6q#0c+_2b#%1+t6lmw&!noy1pgj_u(n^c1sgcry=1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "one-touch-85iz.onrender.com",
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
